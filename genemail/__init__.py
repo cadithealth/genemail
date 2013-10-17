f9c26a229efc0068654f733ca81bc41fb96af519
grabner <pjg.github@ubergrabner.net>
2013-10-17 15:35:22 -0400

    added "genemail.meta" object

--- genemail/__init__.py
+++ genemail/__init__.py
@@ -8,11 +8,21 @@
 #------------------------------------------------------------------------------
 
 from __future__ import absolute_import
+import pkg_resources
 
 from .manager import *
 from .email import *
 from .modifier import *
 from .sender import *
+
+class meta():
+  @property
+  def package(self):
+    return pkg_resources.require('genemail')[0]
+  @property
+  def version(self):
+    return self.package.version
+meta = meta()
 
 #------------------------------------------------------------------------------
 # end of $Id$
